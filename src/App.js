25ef4d3caba0233947c457d28efc457679997944
juliettewooden <72091799+juliettewooden@users.noreply.github.com>
2020-12-15 14:00:38 -0600

    added link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <h1> Weather App </h1>
+      <footer>
+        Project coded by Juliette Wooden on
+          <a href="https://github.com/juliettewooden/react-weather-project"
+        target="blank">
+          Github </a>
+      </footer>
     </div>
   );
 }
 
-export default App;
