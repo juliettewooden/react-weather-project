8583f4818f859af40b724afbc0eae7c23e71e9f1
juliettewooden <72091799+juliettewooden@users.noreply.github.com>
2020-12-15 21:12:05 -0600

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-
+import React from "react";
 import './App.css';
+import Search from "./Search";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1> Weather App </h1>
+      <Search />
       <footer>
-        Project coded by Juliette Wooden on
+        Project coded by Juliette Wooden on{" "} 
           <a href="https://github.com/juliettewooden/react-weather-project"
         target="blank">
           Github </a>
       </footer>
+      </div>
     </div>
   );
 }
