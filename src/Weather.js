346f9ae2b29f6ee3b81cd4a58102f7575984e921
juliettewooden <72091799+juliettewooden@users.noreply.github.com>
2020-12-27 17:06:18 -0600

    completed main apax

--- src/Weather.js
+++ src/Weather.js
@@ -1,19 +1,25 @@
 import React, { useState } from "react";
 import "./Weather.css";
+import axios from "axios";
 
 
 export default function Weather() {
-let [temperature, setTemperature] = useState(null);
+    let [weatherData, setWeatherData] = useState({ ready: false });
     function handleResponse(response) {
         console.log(response.data);
-        setTemperature(Math.round(response.data.main.temp))
-
+        setWeatherData({
+            ready: true,
+            temperature: response.data.main.temp,
+            wind: response.data.wind.speed,
+            city: response.data.name,
+            description: response.data.weather[0].description,
+            humidity: response.data.main.humidity,
+            date: "Monday 09:00"
+        });
     }
 
-let apiKey = "2e65e506db0f4f57568714e0acc190bb";
-let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
-axios.get(url).then(handleResponse);
-    return (
+    if(weatherData.ready) {
+        return (
         <div className="Weather">
             <form>
             <div className="row">
@@ -25,26 +31,45 @@ axios.get(url).then(handleResponse);
                 </div>
             </div>
             </form>
-            <h1> Houston, TX </h1>
+            <h1>{weatherData.city}</h1>
             <ul>
-                <li>Monday 09:00 </li>
-                <li>Cloudy </li>
+                <li>{weatherData.date} </li>
+                <li className="text-capitalize">{weatherData.description} </li>
             </ul>
             <div className="row"> 
             <div className="col-6">
                 <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="Cloudy" />
-                73°F
+                <div className="float-left">
+                    <span className="temp">
+                    {Math.round(weatherData.temperature)}
+                    </span>
+                    <span className="unit">
+                        °F
+                    </span>
+                </div>
+                
+                
             </div>
             <div className="col-6">
                 <ul> 
-                    <li>Preciipitation: 0%</li>
-                    <li>Humidity: 73%</li>
-                    <li>Wind: 12mph</li>
+                    <li>Humidity: {weatherData.humidity}%</li>
+                    <li>Wind: {weatherData.wind}mph</li>
                 </ul>
 
             </div>
             </div>
 
         </div>
-    )
+    );
+} else{
+    let apiKey = "2e65e506db0f4f57568714e0acc190bb";
+    let city = "Houston";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+axios.get(url).then(handleResponse);
+return "Loading..."
 }
+}
+
+
+    
