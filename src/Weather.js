9d87bc08d145c0faf6c35f85af9094013ebbd799
juliettewooden <72091799+juliettewooden@users.noreply.github.com>
2020-12-29 19:00:52 -0600

    adding forecast

--- src/Weather.js
+++ src/Weather.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 
 
 export default function Weather(props) {
@@ -52,6 +53,7 @@ export default function Weather(props) {
             </div>
             </form>
             <WeatherInfo data={weatherData} />
+            <WeatherForecast city={weatherData.city} />
             
         </div>
     );
---
+++ src/WeatherForecast.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import axios from "axios";
+import WeatherForecastPreview from "./WeatherForecastPreview";
+
+export default function WeatherForecast(props) {
+const [forecast, setForecast] = useState(null);
+const [loaded, setLoaded] = useState(false);
+
+
+function revealForecast(response) {
+    console.log(response.data);
+    setForecast(response.data);
+    setLoaded(true);
+    return null;
+    }
+    
+if (loaded) {
+    <div className="WeatherForecast row">
+    <WeatherForecastPreview data={forecast.list[0]} />
+    <WeatherForecastPreview data={forecast.list[1]} />
+    <WeatherForecastPreview data={forecast.list[2]} />
+    <WeatherForecastPreview data={forecast.list[3]} />
+    <WeatherForecastPreview data={forecast.list[4]} />
+    </div>
+} else {
+    let apiKey = "2e65e506db0f4f57568714e0acc190bb";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${props.city}&appid=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(revealForecast);
+
+    return null;
+    
+}
+
+
+
+}
+    
